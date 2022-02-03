e895db23620ba38adc75d053167a8a8b2983c28f
Prabesh <prabesh.bista369@gmail.com>
2022-02-03 09:07:03 +0545

    Gesture Detector

--- lib/custom_painter.dart
+++ lib/custom_painter.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Color _color = Colors.white;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Tab Bar"),
+        title: const Text("Tab Bar"),
+      ),
+      body: Container(
+        color: _color,
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              _color = Colors.red;
+            });
+          },
+        ),
       ),
     );
   }
