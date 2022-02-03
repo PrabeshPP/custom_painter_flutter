afa1e2397ff6a9aaea030a3fd4209083a5caf7ce
Prabesh <prabesh.bista369@gmail.com>
2022-02-03 10:34:08 +0545

    animation

---
+++ lib/animatedCont.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LogoApp extends StatefulWidget {
+  const LogoApp({ Key? key }) : super(key: key);
+
+  @override
+  _LogoAppState createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
