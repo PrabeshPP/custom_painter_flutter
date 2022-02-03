c0924bc75d749fb257349b3c1b1444d18f979d8b
Prabesh <prabesh.bista369@gmail.com>
2022-02-03 09:01:34 +0545

    tab_bar

--- lib/custom_painter.dart
+++ lib/custom_painter.dart
@@ -1,59 +1,14 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 
-class Homepage extends StatefulWidget {
-  const Homepage({Key? key}) : super(key: key);
-
-  @override
-  State<Homepage> createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage>
-    with SingleTickerProviderStateMixin {
-  AnimationController? animationController;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(milliseconds: 500));
-  }
-
-  void toggle() => animationController!.isDismissed
-      ? animationController!.forward()
-      : animationController!.reverse();
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    const double maxSlide = 225.0;
-    var myDrawer = Container(
-      color: Colors.blue,
-    );
-    var myChild = Container(
-      color: Colors.yellow,
-    );
-    return GestureDetector(
-      onTap: toggle,
-      child: AnimatedBuilder(
-          animation: animationController!,
-          builder: (context, _) {
-            double slide = maxSlide * animationController!.value;
-            double scale = 1 - (animationController!.value * 0.3);
-            return Stack(
-              children: <Widget>[
-                myDrawer,
-                Transform.translate(
-                  offset: Offset(maxSlide*animationController!.value,0),
-                  child: Transform(
-                      transform: Matrix4.identity()
-                      ..setEntry(3, 2,0.001)
-                        ..rotateY(-math.pi/2*animationController!.value),
-                      alignment: Alignment.centerLeft,
-                      child: myChild),
-                )
-              ],
-            );
-          }),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tab Bar"),
+      ),
     );
   }
 }
