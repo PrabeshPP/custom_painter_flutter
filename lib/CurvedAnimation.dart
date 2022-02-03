2cff552401ed7c5b3eb07b4e75f56e97e1957ed4
Prabesh <prabesh.bista369@gmail.com>
2022-02-03 16:41:55 +0545

    Curved Animation

---
+++ lib/CurvedAnimation.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class LogoApp2 extends StatefulWidget {
+  const LogoApp2({ Key? key }) : super(key: key);
+
+  @override
+  _LogoApp2State createState() => _LogoApp2State();
+}
+
+class _LogoApp2State extends State<LogoApp2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
