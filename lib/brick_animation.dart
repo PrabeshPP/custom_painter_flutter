9d57e6f2821b855b85c9e055f21ae04fa037eb5d
Prabesh <prabesh.bista369@gmail.com>
2022-02-04 14:53:25 +0545

    Brick_animation

---
+++ lib/brick_animation.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BrickAnimation extends StatefulWidget {
+  const BrickAnimation({ Key? key }) : super(key: key);
+
+  @override
+  _BrickAnimationState createState() => _BrickAnimationState();
+}
+
+class _BrickAnimationState extends State<BrickAnimation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
