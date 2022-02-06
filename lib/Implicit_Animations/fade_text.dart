ab8ca46ef23f10e93aea89311657a458425a89ec
Prabesh <prabesh.bista369@gmail.com>
2022-02-06 10:40:55 +0545

    fade in implicit animations

--- lib/Implicit_Animations/fade_text.dart
+++ lib/Implicit_Animations/fade_text.dart
@@ -25,20 +25,6 @@ class _FadeTextState extends State<FadeText> {
               child: Image.network(
                 owlUrl,
                 fit: BoxFit.cover,
-                loadingBuilder: (BuildContext context, Widget child,
-                    ImageChunkEvent? loadingProgress) {
-                  if (loadingProgress == null) {
-                    return child;
-                  }
-                  return Center(
-                    child: CircularProgressIndicator(
-                      value: loadingProgress.expectedTotalBytes != null
-                          ? loadingProgress.cumulativeBytesLoaded /
-                              loadingProgress.expectedTotalBytes!
-                          : null,
-                    ),
-                  );
-                },
               ),
             ),
           ),
