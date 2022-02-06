ce7fe95af0f4f374e3983b293757c93678725bb1
Prabesh <prabesh.bista369@gmail.com>
2022-02-06 10:34:04 +0545

    fade in implicit animations

--- lib/Implicit_Animations/fade_text.dart
+++ lib/Implicit_Animations/fade_text.dart
@@ -14,15 +14,54 @@ class _FadeTextState extends State<FadeText> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          height: 60,
-          width: 80,
-          child: Image.network(
-            owlUrl,
-            fit: BoxFit.cover,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        
+        children: [
+          Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.3,
+              width: MediaQuery.of(context).size.width * 0.78,
+              child: Image.network(
+                owlUrl,
+                fit: BoxFit.cover,
+                loadingBuilder: (BuildContext context, Widget child,
+                    ImageChunkEvent? loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(
+                      value: loadingProgress.expectedTotalBytes != null
+                          ? loadingProgress.cumulativeBytesLoaded /
+                              loadingProgress.expectedTotalBytes!
+                          : null,
+                    ),
+                  );
+                },
+              ),
+            ),
           ),
-        ),
+          TextButton(
+              onPressed: () {
+                 null;
+              },
+              child:const Text(
+                "Show details",
+                style: TextStyle(color: Colors.blueAccent),
+              )),
+
+        Container(
+          child: Column(
+            children:const  [
+              Text("Type:Owl"),
+              Text("Age:39"),
+              Text("Emplyoment:None")
+
+            ],
+          ),
+        )
+        ],
       ),
     );
   }
