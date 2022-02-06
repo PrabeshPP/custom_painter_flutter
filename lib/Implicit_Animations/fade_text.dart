aea4ab63f6a5148ccf58f91ed392370431b2342d
Prabesh <prabesh.bista369@gmail.com>
2022-02-06 10:08:00 +0545

    fade in

---
+++ lib/Implicit_Animations/fade_text.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const owl_url = 'https://raw.githubusercontent.com/flutter/website/master/src/images/owl.jpg';
+class FadeText extends StatefulWidget {
+  const FadeText({ Key? key }) : super(key: key);
+
+  @override
+  _FadeTextState createState() => _FadeTextState();
+}
+
+class _FadeTextState extends State<FadeText> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
