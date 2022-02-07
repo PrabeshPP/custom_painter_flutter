abda268d9959ecace3df42a2c1500677d2fb10a1
Prabesh <prabesh.bista369@gmail.com>
2022-02-07 12:40:21 +0545

    design1.1

---
+++ lib/Design1.1/slidingcard.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SlidingCardsView extends StatefulWidget {
+  const SlidingCardsView({ Key? key }) : super(key: key);
+
+  @override
+  _SlidingCardsViewState createState() => _SlidingCardsViewState();
+}
+
+class _SlidingCardsViewState extends State<SlidingCardsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
