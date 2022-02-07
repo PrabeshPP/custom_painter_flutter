f866f9f74d9f72e37dbd687d3d75cf1aa18cc1a6
Prabesh <prabesh.bista369@gmail.com>
2022-02-07 14:52:29 +0545

    tab bar

--- lib/Design1.1/slidingcard.dart
+++ lib/Design1.1/slidingcard.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:custom_painter_flutter/Design1.1/tab_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -13,9 +14,9 @@ class _SlidingCardsViewState extends State<SlidingCardsView> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-       const Tabs(),
+        const Tabs(),
         SizedBox(
-          height: MediaQuery.of(context).size.height*0.55,
+          height: MediaQuery.of(context).size.height * 0.55,
           child: const SlidingCard(
             name: 'Shenzhen GLOBAL DESIGN AWARD 2018',
             date: "4.20-30",
@@ -40,29 +41,55 @@ class SlidingCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Card(
-      color: Colors.red,
-      margin:const  EdgeInsets.only(left: 8,right: 8,bottom: 24,top: 14),
+    
+    return Card(
+      margin: const EdgeInsets.only(left: 8, right: 8, bottom: 24, top: 14),
       elevation: 8,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32.0)),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-        ClipRRect(
-        
-          borderRadius:const  BorderRadius.vertical(top:Radius.circular(32.0)),
-          child: Image.asset(assetName,
-              
-              height: MediaQuery.of(context).size.height*0.3,
-              fit: BoxFit.none,),
-            
-        ),
-
-        Text(name),
-     const SizedBox(
-        height: 8,
-      ),
-      Expanded(child: 
-      Container())
+          ClipRRect(
+            borderRadius:
+                const BorderRadius.vertical(top: Radius.circular(32.0)),
+            child: Image.asset(
+              assetName,
+              height: MediaQuery.of(context).size.height * 0.3,
+              fit: BoxFit.none,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text(
+              name,
+              style: const TextStyle(fontSize: 25, color: Colors.black),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, top: 8.0),
+            child: Text(
+              date,
+              style: const TextStyle(color: Colors.black54),
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blueGrey.shade900)
+                ),
+                  onPressed: () {
+                   
+                  },
+                  child:const Text("Reserve",
+                  style: TextStyle(
+                    color: Colors.white
+                  ),))
+            ],
+          )
         ],
       ),
     );
