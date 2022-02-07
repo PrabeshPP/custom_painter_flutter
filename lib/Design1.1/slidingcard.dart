ac39c6d2b5ffc85a09deab21b15da2e47cea80ed
Prabesh <prabesh.bista369@gmail.com>
2022-02-07 12:58:10 +0545

    .

--- lib/Design1.1/slidingcard.dart
+++ lib/Design1.1/slidingcard.dart
@@ -10,11 +10,12 @@ class SlidingCardsView extends StatefulWidget {
 class _SlidingCardsViewState extends State<SlidingCardsView> {
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      child: SlidingCard(
+    return SizedBox(
+      height: MediaQuery.of(context).size.height*0.55,
+      child: const SlidingCard(
         name: 'Shenzhen GLOBAL DESIGN AWARD 2018',
         date: "4.20-30",
-        assetName: "steve-johsom.jpeg",
+        assetName: "assets/steve-johnson.jpeg",
       ),
     );
   }
@@ -33,8 +34,24 @@ class SlidingCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.only(left: 8),
+    return  Card(
+      color: Colors.red,
+      margin:const  EdgeInsets.only(left: 8,right: 8,bottom: 24),
+      elevation: 8,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32.0)),
+      child: Column(
+        children: [
+        Image.asset(assetName,
+      
+      height: MediaQuery.of(context).size.height*0.3,
+      fit: BoxFit.none,),
+     const SizedBox(
+        height: 8,
+      ),
+      Expanded(child: 
+      Container())
+        ],
+      ),
     );
   }
 }
