feb7b4861e2a84a2d3d322e74a0ec6644ac00394
Prabesh <prabesh.bista369@gmail.com>
2022-02-07 12:15:33 +0545

    physics animation

--- lib/AnimationType/physics_animation.dart
+++ lib/AnimationType/physics_animation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
 
 class DragableCard extends StatefulWidget {
   const DragableCard({Key? key}) : super(key: key);
@@ -7,10 +8,20 @@ class DragableCard extends StatefulWidget {
   _DragableCardState createState() => _DragableCardState();
 }
 
-class _DragableCardState extends State<DragableCard> {
+class _DragableCardState extends State<DragableCard>
+    with SingleTickerProviderStateMixin {
+  Alignment _dragElement = Alignment.center;
   late AnimationController controller;
+  late Animation animation;
   @override
   void initState() {
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
+    controller.addListener(() {
+      setState(() {
+        _dragElement = animation.value;
+      });
+    });
     super.initState();
   }
 
@@ -20,12 +31,43 @@ class _DragableCardState extends State<DragableCard> {
     super.dispose();
   }
 
+  void _runAnimation(Offset pixelPerSecond, Size size) {
+    animation = controller
+        .drive(AlignmentTween(begin: _dragElement, end: Alignment.center));
+
+    final unitsPerSecondX = pixelPerSecond.dx / size.width;
+    final unitsPerSecondY = pixelPerSecond.dy / size.height;
+    final unitsPerSecond = Offset(unitsPerSecondX, unitsPerSecondY);
+    final unitVelocity = unitsPerSecond.distance;
+
+    const spring = SpringDescription(mass: 30, stiffness: 1, damping: 1);
+    final simulation = SpringSimulation(spring, 0, 1, -unitVelocity);
+    controller.animateWith(simulation);
+
+    
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Align(
-        alignment: Alignment.center,
-        child: Card(child: FlutterLogo()),
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: GestureDetector(
+        onPanDown: (details) {
+          controller.stop();
+        },
+        onPanUpdate: (details) {
+          setState(() {
+            _dragElement += Alignment(details.delta.dx / (size.width / 2),
+                details.delta.dy / (size.width / 2));
+          });
+        },
+        onPanEnd: (details) {
+          _runAnimation(details.velocity.pixelsPerSecond, size);
+        },
+        child: Align(
+          alignment: _dragElement,
+          child: const Card(child: FlutterLogo()),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:custom_painter_flutter/AnimationType/physics_animation.dart';
 import 'package:custom_painter_flutter/Implicit_Animations/fade_text.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: FadeText(),
+      home: DragableCard(),
       
     );
   }
