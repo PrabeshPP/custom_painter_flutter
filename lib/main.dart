bc54af898799ffd8e53db00eb5d2ff6824002fdd
Prabesh <prabesh.bista369@gmail.com>
2022-02-08 10:16:26 +0545

    parallax effect

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:custom_painter_flutter/Design1.1/mainPage1.dart';
-import 'package:custom_painter_flutter/alignment.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Page101(),
+      home: DesignPage1(),
       
     );
   }
