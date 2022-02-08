edc4809c67a866daabd8b1bded2c54021776f3a8
Prabesh <prabesh.bista369@gmail.com>
2022-02-08 09:52:56 +0545

    testing the alignment

---
+++ lib/alignment.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Page101 extends StatelessWidget {
+  const Page101({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
