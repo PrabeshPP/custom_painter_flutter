19798750a4b95f3e503d79af75a57d17c4ded481
Prabesh <prabesh.bista369@gmail.com>
2022-02-08 10:06:13 +0545

    testing alignment

--- lib/alignment.dart
+++ lib/alignment.dart
@@ -5,13 +5,25 @@ class Page101 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: Image(image:const AssetImage("assets/steve-johnson.jpeg"),
-      height: MediaQuery.of(context).size.height*0.55,
-      width: MediaQuery.of(context).size.width,),
-      
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child:  Column(
+          children: [
+            Image(image: const AssetImage("assets/steve-johnson.jpeg",
+            ),
+            height:MediaQuery.of(context).size.height*0.4,
+            width: MediaQuery.of(context).size.width*0.5,
+            alignment: Alignment(-1.6, 0),
+            fit: BoxFit.none,
+            
+              ),
+          ],
+        ),
+        
+      ),
     );
   }
 }
