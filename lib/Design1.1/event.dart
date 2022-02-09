12f38981cf84b01eaa1145509018972b15305f15
Prabesh <prabesh.bista369@gmail.com>
2022-02-09 10:21:09 +0545

    Added the Events

--- lib/Design1.1/event.dart
+++ lib/Design1.1/event.dart
@@ -1,4 +1,22 @@
 class Event {
   final String imageName;
-  
+  final String title;
+  final String date;
+
+  Event({required this.imageName, required this.title, required this.date});
 }
+
+final List<Event> events = [
+  Event(
+      imageName: "assets/steve-johnson.jpeg",
+      title: "Shenzhen Global Design Award 2018",
+      date: "4..20-30"),
+  Event(
+      imageName: "assets/radion-kutsaev.jpeg",
+      title: "Dawn District,Guandong Honkong and Macao",
+      date: "4.28-31"),
+  Event(
+      imageName: "assets/efe-kurnaz.jpg",
+      title: "Shenzhen Global Design Award 2018",
+      date: "4.20-30")
+];
