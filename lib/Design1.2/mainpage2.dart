2b22743357b10c2e374833fb2632105a9eb2c414
Prabesh <prabesh.bista369@gmail.com>
2022-02-10 09:25:53 +0545

    Ui Design Challenge

--- lib/Design1.2/mainpage2.dart
+++ lib/Design1.2/mainpage2.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({ Key? key }) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: const [
+            Icon(Icons.airplane_ticket)
+
+          ],
+        ),
+      ),
     );
   }
 }
