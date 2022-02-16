403f9078f8ad8383f1586aab3ab369c875d7a80a
Prabesh <prabesh.bista369@gmail.com>
2022-02-16 09:25:29 +0545

    Advanced UI

--- lib/CustomPainter/custom1.dart
+++ lib/CustomPainter/custom1.dart
@@ -6,13 +6,35 @@ class CustomPainter1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ClipPath(
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.3,
-          width: MediaQuery.of(context).size.width,
-          color: Colors.red,
+      body: Center(
+        child: ClipPath(
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.6,
+            width: MediaQuery.of(context).size.width * 0.8,
+            color: Colors.red,
+          ),
+          clipper: MyCustomClipper(),
         ),
       ),
     );
+  }
+}
+
+class MyCustomClipper extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    Path path = Path();
+    // path.moveTo(0, size.height * 0.33);
+    // path.lineTo(0, size.height);
+    // path.lineTo(size.width, size.height);
+    // path.lineTo(size.width, 0);
+    path.quadraticBezierTo(
+        size.width/2, size.height, size.width, size.height / 2);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper oldClipper) {
+    return false;
   }
 }
